929d37fec352a0e53067e686e17de5261c199e29
houkensjtu <houkensjtu@gmail.com>
2016-04-05 23:40:06 +0900

    Modified script to select specific model from a list of all.

--- lenovo_x1carbon.py
+++ lenovo_x1carbon.py
@@ -19,9 +19,13 @@ def getHtml(url):
 def getTitle(html):
      try:
          bsobj = BeautifulSoup(html.read(),"html.parser")
+         
          # Find final price information by searching for following "class" attribute.
-         title = bsobj(attrs= {"class","pricingSummary-details-final-price"})
-
+         # The first call to find_all will return a LIST of prices of all models.
+         # [0] means "to select the first model in the list".
+         # Then the second call to find, return the actual text containing the specified price information.
+         title = bsobj.find_all("li", {"class","tabbedBrowse-productListing-container"})[0].find("dd", {"class","pricingSummary-details-final-price"})
+         
      # If the attribute doesn't exist.
      except AttributeError as e:
          print("Required title not found...")
@@ -30,10 +34,13 @@ def getTitle(html):
              return None
          else:
              return title
+         
 # The following URL is the webshop page of Lenovo Japan for Thinkpad X1-carbon.
 html = getHtml("http://shopap.lenovo.com/jp/notebooks/thinkpad/x-series/x1-carbon/")
 
 # Print all the price information found on this page...
 title = getTitle(html)
-for line in title:
-    print(line)
+
+# print(title) will return all the Tags and attributes as in raw html source.
+# Since what we want is the price information, only, use the get_text() method to wipe out unnecessary info.
+print(title.get_text())
