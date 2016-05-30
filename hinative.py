6c5538ab299713c80deefc8ecdb1a2d574a94b06
Hou Ken / Bao Qian <houkensjtu@gmail.com>
2016-05-30 17:04:26 +0900

    Update hinative.py
    Since https proxy is necessary for opening "https://" address

--- hinative.py
+++ hinative.py
@@ -1,5 +1,7 @@
 from urllib import urlopen
 
-proxies = {'http':'http://ch40203515%40shi-g.com:2N%21n9Ly%24@tnsproxy.shi.co.jp:8080'}
+proxies = {'http':'http://ch40203515%40shi-g.com:2N%21n9Ly%24\
+          @tnsproxy.shi.co.jp:8080','https':'http://ch4020351\
+          5%40shi-g.com:2N%21n9Ly%24@tnsproxy.shi.co.jp:8080'}
 
 urlopen('https://hinative.com/en-US',proxies=proxies)
