c0b3ee842a2538885ff441fdd15b125d05957a4a
bao <qian.bao@shi-g.com>
2016-07-04 11:55:00 +0900

    Now ask user for his/her user account.

--- kintai.py
+++ kintai.py
@@ -1,13 +1,15 @@
 import requests
 # Enter your code below:
-params={'syainCode':'', 'RidIndex':'1', 'password':'','kaisyaCode':'001'}
+code = raw_input('Input your employee code:')
+password = raw_input('Input your Kintai password:')
+params={'syainCode':code, 'RidIndex':'1', 'password':password,'kaisyaCode':'001'}
 s = requests.Session()
 r = s.post('http://jforpe.shi.co.jp/time/timeSheet/loginSyoki.jsp',params = params)
 
-# print r.text
+print(r.text.encode('utf-8'))
 
 # Set your code below, and select month/year.
-params = {'syoriSyainCode':'','nyuryokuSyokaiKubun':'002','kosuScreenFlg':'','setteiNen':'','setteiGetu':''}
+params = {'syoriSyainCode':'','nyuryokuSyokaiKubun':'002','kosuScreenFlg':'','setteiNen':'2016','setteiGetu':'04'}
 r = s.post('http://jforpe.shi.co.jp/time/timeSheet/syokai/tukiSyokai.jsp',params)
 
-print r.text
+print(r.text.encode('utf-8'))
